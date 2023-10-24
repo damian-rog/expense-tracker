18451fc902ee87fa228db03a69dbe2033a257585
damian-rog <damian.rog@quantoo.pl>
2023-10-24 14:09:37 +0200

    own theme examples

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,40 @@
 import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
+var kColorScheme =
+    ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 96, 59, 181));
+
 void main() {
   runApp(
-    const MaterialApp(
-      // theme: ThemeData(useMaterial3: true),
-      home: Expenses(),
+    MaterialApp(
+      //widget for setting up your app theme
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+          centerTitle: false,
+        ),
+        // for example: by this next line you can modify styles of all Card widgets in whole app
+        cardTheme: const CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: kColorScheme.onSecondaryContainer,
+                fontSize: 14,
+              ),
+            ),
+      ),
+      home: const Expenses(),
     ),
   );
 }
